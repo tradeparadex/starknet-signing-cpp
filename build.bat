147b8aa70c0f8021c29e82078d4fa3d7e5f9097c
edwin <edwinswatpako@gmail.com>
2023-11-16 15:05:39 +0300

    Use users parameters

--- build.bat
+++ build.bat
@@ -5,10 +5,10 @@ mkdir build
 
 REM Run conan to install libcurl
 cd libs\curlpp
-conan install . --build=missing -s compiler="Visual Studio" -s build_type=Release -s compiler.version=16
+conan install . --build=missing -s compiler="Visual Studio" -s build_type=Release -s compiler.version=17
 cd ..\..\build
 
 REM Generate Visual Studio project files for the whole project
-cmake -G "Visual Studio 16 2019" -DCMAKE_BUILD_TYPE=Release ..
+cmake -G "Visual Studio 17 2022" -DCMAKE_BUILD_TYPE=Release ..
 REM Build the whole project using Visual Studio's MSBuild
 cmake --build . --target test
